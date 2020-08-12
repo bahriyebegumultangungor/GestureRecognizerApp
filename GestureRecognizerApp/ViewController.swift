ea72fe4f005bf4e9447a3ed91f5c49d449b3aeec
Bahriye Begum Ultan Gungor <begumultan@gmail.com>
2020-08-12 17:20:43 +0300

    First commit

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var isJames = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
-
-
+    
+    @objc func changePic() {
+        
+        if isJames == true {
+            isJames = false
+            imageView.image = UIImage (named: "hetfield")
+            myLabel.text = "Hetfield"
+        } else{
+            isJames = true
+            imageView.image = UIImage (named: "james")
+            myLabel.text = "James"
+        }
+    }
 }
+
+
+
+
+
+
 
